4f2b462fecc4e9560561deba6f1e23406f1cbcc6
vzbel <142253332+vzbel@users.noreply.github.com>
2025-12-29 12:49:22 -0700

    Complete exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,16 +6,20 @@ const Button = ({onClick, text}) => (
 );
 
 // Display cafe ratings
-const Display = ({good, neutral, bad}) => {
+const Display = ({good, neutral, bad, all, avg, positivePct}) => {
   return (
     <div>
       <h2>statistics</h2>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {avg}</p>
+      <p>positive: {positivePct} %</p>
     </div>
   );
 } 
+
 
 const App = () => {
   // Clicks for each btn
@@ -27,13 +31,47 @@ const App = () => {
   const handleNeutral = () => setNeutral(neutral + 1);
   const handleBad = () => setBad(bad + 1);
 
+  const weights = {
+    good: 1,
+    neutral: 0,
+    bad: -1
+  };
+
+  const calculateTotal = () => {
+    return good + neutral + bad;
+  }
+
+  const calculateRatingAvg = () => {
+    const total = calculateTotal();
+    if(total === 0){
+      return 0;
+    }
+    return ((good * weights.good + neutral * weights.neutral +
+    bad * weights.bad) / total)
+  };
+
+  const calculatePositivePct = () => {
+    const total = calculateTotal();
+    if(total === 0){
+      return 0;
+    }
+    return good / total * 100.0;
+  };
+
   return (
     <div>
       <h1>give feedback</h1>
       <Button onClick={handleGood} text="good"/>
       <Button onClick={handleNeutral} text="neutral"/>
       <Button onClick={handleBad} text="bad"/>
-      <Display good={good} neutral={neutral} bad={bad} />
+      <Display 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        all={calculateTotal()}
+        avg={calculateRatingAvg()}
+        positivePct={calculatePositivePct()}
+      />
     </div>
   );
 };
