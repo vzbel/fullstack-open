cd3a5eb1e47b01b9dab4165f6c24703dd0ab0b14
vzbel <142253332+vzbel@users.noreply.github.com>
2025-12-30 10:25:08 -0700

    Complete exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = ({ parts }) => (
+  <div>
+    {
+      parts.map((part) => (
+        <Part key={part.id} part={part}/>
+      ))
+    }
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const Course = ({ course }) => {
+  return (
+    <article>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+    </article>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
