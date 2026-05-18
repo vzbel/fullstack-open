992b4dbcd3d8bc23c417c1336bc249ff053f4c1b
vzbel <142253332+vzbel@users.noreply.github.com>
2026-05-18 09:31:48 -0600

    Complete exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -31,7 +31,10 @@ const app = express();
 app.use(express.json());
 
 // logging middleware
-app.use(morgan("tiny"));
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
